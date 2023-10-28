dcca94a25ff2df12366ba967d87dcecb232f2c3b
nagakowshik222 <149249234+nagakowshik222@users.noreply.github.com>
2023-10-28 19:08:06 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the numbers, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "The Battle of Plassey was fought in?",
+        answer: "1757",
+        options: [
+            "1757",
+            "1782",
+            "1748",
+            "1764"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Tripitakas are sacred books of ?",
+        answer: "buddhist",
+        options: [
+            "buddhist",
+            "hindus",
+            "jains",
+            "none of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "The power to decide an election petition is vested in the ?",
+        answer: "election commission",
+        options: [
+            "parliament",
+            "supreme court",
+            "high court",
+            "election commission"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is used in pencils?",
+        answer: "graphite",
+        options: [
+            "graphite",
+            "silicon",
+            "charcoal",
+            "phosphorous"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The first hang gliders to be flown were flown in...?",
+        answer: "1800s",
+        options: [
+            "1600s",
+            "1700s",
+            "1800s",
+            "1900s"
+        ]
+    },
+];
